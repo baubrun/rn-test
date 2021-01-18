d07388ab9d633c7ee4c5072e57d25f62ff812f6f
baubrun <baubelf@gmail.com>
2021-01-18 17:32:15 -0500

    3 boxes

--- App.js
+++ App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.box}>
+        <Text style={styles.boxText}> #1</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.boxText}> #2</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.boxText}> #3</Text>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "ghostwhite",
+    ...Platform.select({
+      ios: { paddingTop: 20 },
+      android: { paddingTop: StatusBar.currentHeight },
+    }),
+  },
+  box: {
+    width: 300,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderColor: "darkslategray",
+  },
+  boxText: {
+    color: "darkslategray",
+    fontWeight: "bold",
   },
 });
+
+
